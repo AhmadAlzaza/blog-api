7475cb9b86d9613dededd267278ca6a527a1dfb9
AhmadAlzaza <ahmad.alzaza00@gmail.com>
2026-03-16 03:57:06 +0300

    add UserResource

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
@@ -21,7 +22,7 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;
         return response()->json([
             'token' => $token,
-            'user' => $user,
+            'user' => new UserResource($user),
             'message' => 'Register has been successfully'
         ], 201);
     }
